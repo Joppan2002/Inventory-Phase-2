53713290be58cd5079ca2f1e38602b9fd04a9d35
Augustine Joel Joseph <115809487+Joppan2002@users.noreply.github.com>
2025-09-11 09:24:47 +0530

    Update InventoryDAO.java

--- InventoryManagement/src/com/litmus7/dao/InventoryDAO.java
+++ InventoryManagement/src/com/litmus7/dao/InventoryDAO.java
@@ -6,29 +6,21 @@ import java.sql.SQLException;
 import java.util.List;
 
 import com.litmus7.dto.Inventory;
-import com.litmus7.exceptions.InventoryDAOException;
-import com.litmus7.constants.errorCode;
-
 
 public class InventoryDAO {
 
     private static final String INSERT_SQL =
-        "INSERT INTO inventory (SKU, ProductName, Quantity, Price) VALUES (?, ?, ?, ?)";
+        "INSERT INTO inventory (SKU, ProductName, quantity, price) VALUES (?, ?, ?, ?)";
 
 
 
-    public static void insert(Inventory item, Connection conn) throws InventoryDAOException
-    {
+    public void insert(Inventory item, Connection conn) throws SQLException {
         try (PreparedStatement ps = conn.prepareStatement(INSERT_SQL)) {
             ps.setInt(1, item.getSKU());
             ps.setString(2, item.getProductName());
             ps.setInt(3, item.getQuantity());
             ps.setDouble(4, item.getPrice());
             ps.executeUpdate();
-        }
-        catch(Exception e)
-        {
-        	throw new InventoryDAOException("ErrorCode.DB",errorCode.DB,e);
         }
     }
 
