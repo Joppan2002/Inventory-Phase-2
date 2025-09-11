3d7a9a3963ec5cbef7dd51a33541f37d0a919373
Augustine Joel Joseph <115809487+Joppan2002@users.noreply.github.com>
2025-09-11 09:22:38 +0530

    Update App.java

--- InventoryManagement/src/com/litmus7/InventoryManagement/App.java
+++ InventoryManagement/src/com/litmus7/InventoryManagement/App.java
@@ -2,15 +2,19 @@ package com.litmus7.InventoryManagement;
 
 import com.litmus7.controller.InventoryController;
 
-
 public class App {
     public static void main(String[] args) {
         InventoryController controller = new InventoryController();
 
+        System.out.println("Starting Phase 2 processing (single-threaded)...");
 
-        int processedCount = controller.triggerPhase2Processing();
+        int processedCount[] = controller.triggerPhase2Processing();
 
         System.out.println("Phase 2 processing finished.");
-        System.out.println("Total files submitted: " + processedCount);
+        System.out.println("Total files processed: " + processedCount[0]);
+        System.out.println("Moved to processed: "+processedCount[1]);
+        System.out.println("Moved to error: "+processedCount[2]);
+        
+        
     }
 }
